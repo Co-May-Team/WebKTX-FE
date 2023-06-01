8831af7fea511c90130c04f254f7dc57b5ee2dc7
Nguyễn Võ Song Toàn <adamwilling.2002@gmail.com>
2023-06-02 01:33:02 +0700

    Fix 2

--- src/containers/defaults/RegistrationForm/FamilyInfoForm.js
+++ src/containers/defaults/RegistrationForm/FamilyInfoForm.js
@@ -10,17 +10,17 @@ import FamilyInfoFormItem from "./FamilyInfoFormItem"
 
 const info = {
   status: "",
-  fullName: "",
-  yearOfBirth: "",
-  phoneNumber: "",
+  fullName: null,
+  yearOfBirth: null,
+  phoneNumber: null,
   provinceAddress: null,
   districtAddress: null,
   wardAddress: null,
-  detailAddress: "",
-  currentJob: "",
-  placeOfWork: "",
-  phoneNumberOfCompany: "",
-  income: 0,
+  detailAddress: null,
+  currentJob: null,
+  placeOfWork: null,
+  phoneNumberOfCompany: null,
+  income: null,
 }
 
 const relativeInfo = {
