f90d740a07b236b8f8176b6b232de395bb7fd8fb
Nguyễn Võ Song Toàn <62372475+adamwilling@users.noreply.github.com>
2023-07-03 21:29:46 +0700

    Update PostDetailPage.js

--- src/containers/defaults/Posts/PostDetail/PostDetailPage.js
+++ src/containers/defaults/Posts/PostDetail/PostDetailPage.js
@@ -3,7 +3,7 @@ import moment from "moment"
 import "moment/locale/vi" // Import Moment locale for Vietnamese
 import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
-import { Bounce, Fade, Slide, Zoom } from "react-reveal"
+import { Fade, Slide } from "react-reveal"
 import { useParams } from "react-router-dom"
 import Loading from "~/components/Loading"
 import Motion from "~/components/Motion"
@@ -182,9 +182,7 @@ export default function PostDetailPage(props) {
                     className=' text-neutral-900 font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl '
                     title={postInfo?.title}
                   >
-                    <Bounce right cascade>
-                      {postInfo?.title}
-                    </Bounce>
+                    {postInfo?.title}
                   </h1>
                   <div className='w-full border-b border-neutral-100 dark:border-neutral-800' />
                   <div className='flex flex-col sm:flex-row justify-between sm:items-end space-y-5 sm:space-y-0 sm:space-x-5'>
@@ -378,14 +376,12 @@ export default function PostDetailPage(props) {
           <div className='container flex flex-col my-10 lg:flex-row '>
             <div className='w-full lg:w-3/5 xl:w-2/3 xl:pr-20'>
               <div className='space-y-10'>
-                <Zoom right cascade>
-                  <div
-                    className='prose lg:prose-lg !max-w-screen-md mx-auto dark:prose-invert content-wrapper'
-                    dangerouslySetInnerHTML={{
-                      __html: postInfo?.content,
-                    }}
-                  />
-                </Zoom>
+                <div
+                  className='prose lg:prose-lg !max-w-screen-md mx-auto dark:prose-invert content-wrapper'
+                  dangerouslySetInnerHTML={{
+                    __html: postInfo?.content,
+                  }}
+                />
                 {/* <div className="max-w-screen-md mx-auto flex flex-wrap">
                             <div
                                 className="nc-Tag inline-block bg-white text-sm text-neutral-600 dark:text-neutral-300 py-2 px-3 rounded-lg border border-neutral-100 md:py-2.5 md:px-4 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000 mr-2 mb-2"
